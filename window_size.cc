fff40deb1f2e7dd66dc9a873308a3ab5e7e7dd8f
NandorKovacs <nandor1kovacs@gmail.com>
2022-05-05 17:03:11 +0200

    fixed window resize

--- window_size.cc
+++ window_size.cc
@@ -3,10 +3,10 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
 #include <unordered_map>
 
 #include "lib/errors.h"
-
 
 static std::unordered_map<GLFWwindow*, WindowSizeTracker*> trackers;
 
@@ -26,7 +26,7 @@ WindowSizeTracker::WindowSizeTracker(GLFWwindow* window) : window(window) {
   glfwGetWindowSize(window, &size.x, &size.y);
   CHECK_GLFW("glfwGetWindowSize");
 
-  glfwSetWindowSizeCallback(window, window_size_callback); 
+  glfwSetWindowSizeCallback(window, window_size_callback);
   CHECK_GLFW("glfwSetWindowSizeCallback");
 
   trackers[window] = this;
@@ -37,11 +37,10 @@ void WindowSizeTracker::on_size_changed(int width, int height) {
   size.x = width;
   size.y = height;
   did_change = true;
+  glViewport(0, 0, width, height);
 }
 
-glm::ivec2 WindowSizeTracker::get() const {
-  return size;
-}
+glm::ivec2 WindowSizeTracker::get() const { return size; }
 
 bool WindowSizeTracker::should_update() {
   bool old_did_change = did_change;
