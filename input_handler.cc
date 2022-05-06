94aa4231f12c3684bbf9dff5b2567c5286c6d619
Nandi <nandor1kovacs@gmail.com>
2022-05-06 11:20:34 +0200

    input handler wip

---
+++ input_handler.cc
@@ -0,0 +1,15 @@
+#include "input_handler.h"
+
+#include <GLFW/glfw3.h>
+
+
+exter "C" {
+  static void key_event_callback(GLFWwindow* window) {
+
+  }
+}
+
+
+InputHandler::InputHandler(GLFWwindow* window) {
+  glfwSetKeyCallback(window,)
+}
---
+++ input_handler.h
@@ -0,0 +1,12 @@
+#ifndef VIZ_INPUT_HANDLER
+#define VIZ_INPUT_HANDLER
+
+#include <GLFW/glfw3.h>
+
+class InputHandler {
+ public:
+  InputHandler(GLFWwindow* window);
+ private:
+};
+
+#endif //VIZ_INPUT_HANDLER
