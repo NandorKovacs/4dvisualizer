077b5093f93f265396b69fdd7744af2dee2d3a87
NandorKovacs <nandor1kovacs@gmail.com>
2022-10-11 16:38:06 +0200

    cmake, basic test, edges broken

---
+++ geometry/intersect_test.cc
@@ -0,0 +1,29 @@
+#include "intersect.h"
+
+#include <gtest/gtest.h>
+#include <vector>
+
+namespace viz {
+namespace intersect {
+namespace {
+
+
+
+TEST(IntersectTest, IntersectWithDefaultPlane) {
+  Intersector intersector;
+  Hyperplane plane;
+  plane.pos = glm::vec4(0,0,0,0);
+  plane.normal = glm::vec4(0,0,0,1);
+  
+  std::vector<Triangle> triangles;
+  intersector.intersect([&](Triangle const& t){
+    triangles.push_back(t);
+  },plane);
+  
+  
+  EXPECT_EQ(12, triangles.size());
+}
+
+}  // namespace
+}  // namespace intersect
+}  // namespace viz
